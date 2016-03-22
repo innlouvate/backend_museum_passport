e9ca8259555563a042e4e128423065518ce75a6e
rachelsmithcode <rachelcodesmith@outlook.com>
2016-03-22 16:03:09 +0000

    tests failing, requires amendment to routes

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -8,13 +8,17 @@ RSpec.describe QuestionsController, type: :controller do
     test_museum = Museum.create(name: "Sample Museum")
     test_exhibit = Exhibit.create(name: "Sample Exhibit", museum_id: test_museum.id)
     test_question = Question.create(description: "Sample Question", exhibit_id: test_exhibit.id)
-    get "/museums/test_museum.id/exhibits/test_exhibit.id/questions"
-    json = JSON.parse(response.body)
+    get :index
+    # get "/museums/#{test_museum.id}/exhibits/#{test_exhibit.id}/questions", {}, {"Accept" => "application/json"}
+    p "I am a dog"
+    parsed_body = JSON.parse(response.body)
+    p parsed_body
+    p "i am a cat"
     expect(response).to be_success
-    p json
-    expect(json[0]["museum"]["id"]).to eq(test_museum.id)
-    expect(json[0]["museum"]["answers"][0]["exhibit"]["id"]).to eq(test_museum.id)
+    p parsed_body
+    p "I am not going to print cos I'm sulking"
+    expect(parsed_body.last).to eq(test_question)
+    # expect(parsed_body[0]["museum"]["answers"][0]["exhibit"]["id"]).to eq(test_museum.id)
   end
-
 
 end

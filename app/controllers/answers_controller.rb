dc88e44d60f78a2b14ab89afbceee882d89f26e5
innlouvate <l.j.franklin85@googlemail.com>
2016-03-27 19:50:13 +0100

    added answer update method and reformatted json output

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,8 +7,35 @@ class AnswersController < ApplicationController
   # before_filter :set_headers
   # protect_from_forgery with: :null_session
 
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+
   def create
     @answer = Answer.new(entry: params[:entry], question_id: params[:question_id], user_id: params[:user_id])
+    p @answer
     # @response.user_id = current_user.id
     if @answer.save
       respond_to do |format|
@@ -18,8 +45,8 @@ class AnswersController < ApplicationController
   end
 
   def update
-    @answer = Answer.update(params[:answer_id], entry: params[:entry])
-    if @answer.save
+    if @answer = Answer.update(params[:answer_id], entry: params[:entry])
+      p @answer
       respond_to do |format|
         format.json{render :json => @answer, :status => :updated}
       end
@@ -46,6 +73,7 @@ class AnswersController < ApplicationController
      headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD'
      headers['Access-Control-Allow-Headers'] = '*, x-requested-with, Content-Type, If-Modified-Since, If-None-Match'
      headers['Access-Control-Max-Age'] = '86400'
+
    end
 
 end

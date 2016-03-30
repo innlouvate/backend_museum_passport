9f9bedcf74933ed7393211fea77ecb0c37c1cd58
innlouvate <l.j.franklin85@googlemail.com>
2016-03-30 19:19:09 +0100

    added image field to questions json and accept image paramas when create answer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330154632) do
+ActiveRecord::Schema.define(version: 20160330180147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160330154632) do
     t.datetime "updated_at",  null: false
     t.integer  "question_id"
     t.integer  "user_id"
+    t.text     "image"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
